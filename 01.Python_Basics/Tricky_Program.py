bb1c158cb52e7bf360f5859d0fbedb483e2ac2c5
DevanshAjmera <devanshajmera1012@gmail.com>
2025-10-02 19:43:51 +0530

    File Added

---
+++ 01.Python_Basics/Tricky_Program.py
@@ -0,0 +1,26 @@
+# Square root of a number
+
+a = 25
+print(f"Square root of {a} is { a**(0.5) }")
+
+# By newton method
+"""
+x = root (a)
+x^2 = a
+
+f(a) = x^2 - a
+
+x = x - f(a)/ f'(a)
+x = x - (x^2 - a)/(2x) 
+x = x - (x/2 - a/2x)
+x = x - x/2 + a/2x
+x = x/2 + a/2x
+x = 0.5 * (x + a/x)
+
+
+"""
+x = a
+for i in range (10):
+    x = 0.5 * (x + a/x)
+
+print(x)
